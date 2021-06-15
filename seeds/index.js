4e56840f682ff2c1eba6b2d76e39cd5e90a1efe4
Jessica Stayton <jessica.stayton@gmail.com>
2021-06-14 22:38:05 -0600

    Basic authorization; adding authors and reviews

--- seeds/index.js
+++ seeds/index.js
@@ -23,6 +23,7 @@ const seedDB = async () => {
         const random1000 = Math.floor(Math.random() * 1000);
         const price = Math.floor(Math.random() * 30) + 10;
         const camp = new Campground({
+            author: '60c2b1d8c79cce40f4265fb5',
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
             image: 'http://source.unsplash.com/collection/483251',
